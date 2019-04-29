6788668e79acd05c6125b3b2b2337075f3ef4ae4
Farhan <farhanmahmoodny@gmail.com>
2019-04-29 17:53:50 -0400

    fixed relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :memories
   has_many :photos, through: :memories
   has_many :comments
-  has_many :photos, through: :comments
+  # has_many :photos, through: :comments
 
   validates :email, uniqueness: true
 
-  has_secure_password 
+  has_secure_password
 end
