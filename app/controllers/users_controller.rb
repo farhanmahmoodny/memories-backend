acc3f4da0cd01485daef268456b3782ef68ae23f
Farhan <farhanmahmoodny@gmail.com>
2019-04-30 14:39:58 -0400

    delete user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,22 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(name: params[:name], email: params[:email], username: params[:username], password: params[:password_digest])
-    render json: @user
+    if @user.valid?
+      render json: @user, status: :created
+    else
+      render json: {error: 'failed to create user'}, status: :not_acceptable
+    end
   end
 
   def update
     @user = User.find(params[:id])
     @user.update(name: params[:name], email: params[:email], username: params[:username])
     render json: @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
   end
 
 end
